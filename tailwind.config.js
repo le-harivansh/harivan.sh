436dda7ea576c9b07a4ca83e556200e0d27b65b8
Harivansh <hello@harivan.sh>
2023-07-04 14:59:47 +0400

    chore: port existing website data to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html"
   ],
   theme: {
+    fontFamily: {
+      display: ['Titillium Web', ...defaultTheme.fontFamily.sans],
+      heading: ['Poiret One', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
